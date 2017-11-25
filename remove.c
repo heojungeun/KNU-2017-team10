4930a45bbe9d1327e081c749f76ad4b76963cbbf
Sunik9303 <attmey@naver.com>
2017-11-25 15:07:15 +0900

    rm version1

---
+++ remove.c
@@ -0,0 +1,18 @@
+//rm version1 2017-11-25
+#include<stdio.h>
+#include<unistd.h>
+
+int main(int ac, char *av[])
+{
+	if(ac==1){
+		printf("there is not target file\n");
+		exit(1);
+	}
+	else{
+		if(unlink(av[1])==-1){
+			printf("fail to remove %s\n",av[1]);
+			exit(1);
+		}
+	}
+
+}
