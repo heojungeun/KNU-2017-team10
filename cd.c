1650b78d1e0d9deda3b3df4052960c9d4b5a9003
gytjs9611 <33477060+gytjs9611@users.noreply.github.com>
2017-11-25 19:13:08 +0900

    Add files via upload

---
+++ cd.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<dirent.h>
+#include<sys/types.h>
+
+void cd(int argc, char *argv[])
+{
+
+	if(argc ==1)
+	{
+		chdir(getenv("HOME"));
+		// cd => home으로 이동
+	}
+	else if(argc==2)
+	{
+		if(chdir(argv[1]==-1)
+			printf("No such directory\n");
+		
+		// 해당 dir가 존재하지 않을 경우
+	}
+	else
+	{	
+		printf("usage: cd dirname\n");
+
+		// 형식이 잘못되었을 경우
+	}
+
+}
+			
