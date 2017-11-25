1be129f3341eb23bbff3f67d528cce963a9bf237
Sunik9303 <attmey@naver.com>
2017-11-25 14:16:34 +0900

    mv.c version1

---
+++ mv.c
@@ -0,0 +1,20 @@
+//mv.c version1 2017-11-25
+#include<stdio.h>
+#include<unistd.h>
+
+int main(int ac ,char *av[])
+{	
+	int result;
+	
+	if(ac==1)
+	{
+		exit(1);	
+	}
+	else{
+		if(result=rename(av[1],av[2])==-1){
+			perror("error");
+			exit(1);
+		}
+	}
+
+}
