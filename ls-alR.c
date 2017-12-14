7fe93c244e9f6644b36eb299d67d81f34cd14114
gytjs9611 <33477060+gytjs9611@users.noreply.github.com>
2017-12-14 21:14:12 +0900

    Update ls-alR.c

--- ls-alR.c
+++ ls-alR.c
@@ -10,18 +10,6 @@
 #include<pwd.h>
 #include<time.h>
 
-
-
-char *getString(); // 동적할당으로 문자입력받기
-void checkFile(struct stat *buf, Info_st *info); // 파일권한 검사함수
-int totalSize(char dirname[]); // 디렉토리 크기 구하는 함수
-void sortFile(List *list); // 파일 이름으로 정렬하는 함수
-void printList(List *list); // 리스트에 저장된 자료 출력 함수
-char *copyPath(char *dirname, char *filename); // 파일경로 복사하는 함수
-int saveInStruct(char dirname[]); // 디렉토리 자료를 info_st에 저장하는 함수
-void delete(void *data); // 메모리 해제
-void space(List * list, int max[]); // 공간계산
-
 typedef struct{
   char  permission[11];
   int   linkcount;
@@ -44,15 +32,24 @@ typedef struct element {
 typedef struct List_ {
 
 	int                size;
-
 	int                (*match)(const void *key1, const void *key2);
 	void               (*destroy)(void *data);
-
 	element           *head;
 	element           *tail;
 
-} List;
+}List;
 
+
+
+char *getString(); // 동적할당으로 문자입력받기
+void checkFile(struct stat *buf, Info_st *info); // 파일권한 검사함수
+int totalSize(char dirname[]); // 디렉토리 크기 구하는 함수
+void sortFile(List *list); // 파일 이름으로 정렬하는 함수
+void printList(List *list); // 리스트에 저장된 자료 출력 함수
+char *copyPath(char *dirname, char *filename); // 파일경로 복사하는 함수
+int saveInStruct(char dirname[]); // 디렉토리 자료를 info_st에 저장하는 함수
+void delete(void *data); // 메모리 해제
+void space(List * list, int max[]); // 공간계산
 void list_init(List *list, void (*destroy)(void *data));
 void list_destroy(List *list);
 int list_ins_next(List *list, ListElmt *element, const void *data);
