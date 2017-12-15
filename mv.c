45c920ddaffb0ff3b934016ee76617336d07d864
Sunik9303 <33475114+Sunik9303@users.noreply.github.com>
2017-12-15 13:56:53 +0900

    mv.c
    mv.c ver2 + 설명

--- mv.c
+++ mv.c
@@ -11,14 +11,14 @@ int main(int ac ,char *av[])
 	int r_size,w_size;
 	char buf[100];
 
-	if(ac==1)
+	if(ac==1)//mv command 실행시 파일 이름이 입력되는지 확인
 	{	
 		perror("check the filename");
 		exit(1);	
 	}
-	else{	
-		fd1=open(av[1],O_RDONLY);
-		fd2=open(av[2],O_RDWR|O_CREAT|O_EXCL,0664);
+	else if(ac==3){	
+		fd1=open(av[1],O_RDONLY);//original
+		fd2=open(av[2],O_RDWR|O_CREAT|O_EXCL,0664);//이동된 파일
 
 		r_size=read(fd1,buf,100);
 		w_size=write(fd2,buf,r_size);
@@ -26,7 +26,7 @@ int main(int ac ,char *av[])
 			r_size=read(fd1,buf,100);
 			w_size=write(fd2,buf,r_size);
 		}
-	}
-	unlink(av[1]);
+	}제
+	unlink(av[1]);//원본파일 삭제
 	return 0;
 }
